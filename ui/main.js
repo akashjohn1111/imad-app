1a9b92f3ec9253e05558eee5684e740f94de2ff3
Akash Das <akashjohn1111@gmail.com>
2018-02-17 23:59:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='HAHAHAHA';
 var img=document.getElementById('madi');
-img.onclick=function(){
-img.style.marginLeft="200px";
+img.onclick= function(){
+    
+      img.style.marginLeft="200px";
 };
