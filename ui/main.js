9561cb5e53165a9384eaa115317c079b301c2356
Akash Das <akashjohn1111@gmail.com>
2018-02-18 01:55:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,3 +7,22 @@ img.onclick = function ()
     img.style.marginLeft = '100px';
     
 };
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    if(request.ststus==XMLHTTpRequest.DONE)
+{
+    if(request.readyState===200)
+    {var names=responseText;
+    names=JSON.parse(names);
+    for(var i=0;i<names.length;i++)
+
+        {list+='<li>'+names[i]+'</li>';
+        }
+        var ul=document.getElementById('namelist');
+        ul.innerHTMl=list;
+    }
+    }};
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    request.open('get','akashjohn1111@ssh.imad.hasura-app.io')
+    }
