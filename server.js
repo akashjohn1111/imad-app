6d23a187791e6cd79c03777de1bf27f2f6692d74
Akash Das <akashjohn1111@gmail.com>
2018-02-17 22:24:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-one', function (req, res) {
 app.get('/article-two', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-one', function (req, res) {
+app.get('/article-3', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
 });
 
