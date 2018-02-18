a90f092e96819295191ea63c544bf937ce1d9f4d
Akash Das <akashjohn1111@gmail.com>
2018-02-18 18:44:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var names=[];
-app.get('/submit-name',function(req,res){
-    var name=req.query.names;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
+
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
